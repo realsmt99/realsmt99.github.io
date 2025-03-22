e3cbca147831f8b61fbaaeb8739d595d8b2e2814
Amine Menadi <aminemenadi09@gmail.com>
2025-03-22 16:09:04 +0100

    Update service worker version and change page title

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2168768016"
+    serviceWorkerVersion: "2628796100"
   }
 });
