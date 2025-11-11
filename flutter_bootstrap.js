929ba03955ff884399dc0ac67947ae655f022b2b
Amine MENADI <aminemenadi09@gmail.com>
2025-11-11 16:11:35 +0100

    test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2181694157"
+    serviceWorkerVersion: "2879894434"
   }
 });
